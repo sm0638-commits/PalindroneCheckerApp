aa7439ca3e3058e29da92ad92aa870d11621c89e
saranyaAI <madavanerisaranya@gmail.com>
2026-02-27 00:35:33 +0530

    [sm0638-commits] Added UC12Pattern

---
+++ App/src/UC12Pattern.java
@@ -0,0 +1,28 @@
+public class UC12Pattern{
+    public static void main(String[] args) {
+        String input = "level";
+        PalindromeStrategy strategy = new StackStrategy();
+        boolean result = strategy.check(input);
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
